f9aa1cdd8f74e22306c7a30ca272247dc0ed7c6f
Brian Cunnie <bcunnie@pivotal.io>
2019-01-25 14:13:41 -0800

    wip
    Signed-off-by: Josh Russett <jrussett@pivotal.io>

--- lib/team-edition/tracker_api.rb
+++ lib/team-edition/tracker_api.rb
@@ -19,23 +19,21 @@ module TeamEdition
     def add_to_project(project_id, name, initials, email, role)
       abort "role must be owner, member or viewer" unless %w{owner member viewer}.include?(role)
 
-      uri = URI.parse("https://www.pivotaltracker.com/services/v3/projects/#{project_id}/memberships")
+      uri = URI.parse("https://www.pivotaltracker.com/services/v5/projects/#{project_id}/memberships")
       http = Net::HTTP.new(uri.host, uri.port)
       http.use_ssl = true
       http.verify_mode = OpenSSL::SSL::VERIFY_NONE
       request = Net::HTTP::Post.new(uri.path)
-      request["Content-Type"] = "application/xml"
+      request["Content-Type"] = "application/json"
       request["X-TrackerToken"] = @api_key
-      request.body = <<-XML
-      <membership>
-        <role>#{role}</role>
-        <person>
-          <name>#{name}</name>
-          <initials>#{initials}</initials>
-          <email>#{email}</email>
-        </person>
-      </membership>
-      XML
+      request.body = <<-JSON
+      {
+        "role": "#{role}",
+        "email": "#{email}",
+        "name": "#{name}",
+        "initials": "#{initials}"
+      }
+      JSON
       response = http.request(request)
       raise "Failed: #{response.inspect} #{response.body}" unless response.code == "200"
     end
